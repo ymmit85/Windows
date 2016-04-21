d2f1d5f1c7b571b24b18e2a51d0f4ef423a1be2b
Tim Williams <tim@ymmit.net>
2016-04-21 18:33:38 +0800

    Commit

---
+++ Output_Directory.ps1
@@ -0,0 +1,26 @@
+﻿       <#
+              .SYNOPSIS
+                    Sets output directory for PowerShell sctipt.
+					 
+					 
+              .DESCRIPTION
+                     Can be used within script to allow entry of a path for output, if this in not specified then the current working directory is used for output.
+                     If the parameter -Path is not specified then the current working directory is used, variable $output will contain directory.
+      
+
+       #>
+
+Param
+(
+	[CmdletBinding()]
+	[Parameter(Mandatory=$false)]
+	[string]$path
+)
+
+if ($path) {
+    $output = $path
+    } else { 
+    $output = $PWD
+    }
+
+    Write-Host $output
