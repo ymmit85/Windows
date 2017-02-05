f43d9a7aca69b14f203805f3da962f252db574b3
ymmit85 <ymmit85@users.noreply.github.com>
2017-02-05 09:34:37 +0800

    ScriptHeader.ps1
    PowerShell Header to work with get-help

---
+++ ScriptHeader.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+Example PowerShell header to work with get-help
+
+.DESCRIPTION
+Description of script
+
+.EXAMPLE
+Example of how to run script
+
+.NOTES
+Put some notes here.
+
+.LINK
+URL or Contact details
+
+#>
